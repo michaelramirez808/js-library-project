59d32003d648663cf45d201da71c17254d8b1678
Michael <michael76ramirez@gmail.com>
2023-06-21 21:02:31 -0700

    added array info function

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ function Book(title, author, pages, status){
     this.status = status;
 }
 
+function addArrayToPage(){
+    let libraryContainer = document.getElementById('libraryContainer');
+    libraryContainer.textContent = '';
+    for(let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+
+        let bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+
+        bookDiv.innerHTML = `
+        <h3>Title: ${book.title}</h3>
+        <p>Author: ${book.author}</p
+        <p>Pages: ${book.pages}</p>
+        <p>Status: ${book.status}</p>
+        `
+        libraryContainer.appendChild(bookDiv);
+    }
+}
 
 function addBookToLibrary(){
     let addButton = document.getElementById('add')
@@ -18,7 +36,7 @@ function addBookToLibrary(){
         event.preventDefault();
     let title = document.getElementById('title').value;
     let author = document.getElementById('author').value;
-    let pages = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
     let status = document.getElementById('status').value;
 
     const newBook = new Book(title, author, pages, status);
@@ -28,6 +46,8 @@ function addBookToLibrary(){
     bookForm.reset();
     bookForm.style.display = 'none';
 
+
+    addArrayToPage();
     });
 }
 addBookToLibrary();
